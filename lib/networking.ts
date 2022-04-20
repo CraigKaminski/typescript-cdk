925c68f7f8a2ff88a7dd24e090be59598bc12c3e
Craig Kaminski <craig.j.kaminski@gmail.com>
2022-04-20 11:50:03 -0400

    Module 1-4 changes

---
+++ lib/networking.ts
@@ -0,0 +1,31 @@
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+interface NetworkingProps {
+  maxAzs: number
+}
+
+export class Networking extends Construct {
+  public readonly vpc: ec2.IVpc;
+
+  constructor(scope: Construct, id: string, props: NetworkingProps) {
+    super(scope, id);
+
+    this.vpc = new ec2.Vpc(this, 'AppVPC', {
+      cidr: '10.0.0.0/16',
+      maxAzs: props.maxAzs,
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'Public',
+          cidrMask: 24
+        },
+        {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+          name: 'Private',
+          cidrMask: 24
+        }
+      ]
+    });
+  }
+}
--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,16 +1,25 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Tags, aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Networking } from './networking';
 
 export class TypescriptCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'DocumentsBucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TypescriptCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'DocumentsBucketNameExport', {
+      exportName: 'DocumentsBucketName',
+      value: bucket.bucketName
+    });
+
+    const networkingStrack = new Networking(this, 'NetworkingConstruct', {
+      maxAzs: 2
+    });
+
+    Tags.of(networkingStrack).add('Module', 'Networking');
   }
 }
